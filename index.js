4d04722275906cb87518a941a9a22e82e9b9d1f5
Harikrishnan <sushindhran@gmail.com>
2014-07-16 13:29:39 -0400

    Added copyright comment

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+/*
+ * Copyright (c) 2013 Home Box Office, Inc. as an unpublished
+ * work. Neither this material nor any portion hereof may be copied or
+ * distributed without the express written consent of Home Box Office, Inc.
+ *
+ * This material also contains proprietary and confidential information
+ * of Home Box Office, Inc. and its suppliers, and may not be used by or
+ * disclosed to any person, in whole or in part, without the prior written
+ * consent of Home Box Office, Inc.
+ */
+
 /* jshint strict:false */
 /* jshint camelcase:false */
 /* jshint node:true */
