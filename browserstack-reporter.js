aa828c8a29b141c25db0719ff2a1564a350a668a
Samiran Saha <samiran.s89@gmail.com>
2020-02-07 10:31:34 +0530

    implemented grunt code quality checks for all js files

--- browserstack-reporter.js
+++ browserstack-reporter.js
@@ -1,33 +1,31 @@
 var Browserstack = require('browserstack')
-var common = require("./common")
+var common = require('./common')
 
-
-var BrowserStackReporter = function(logger, /* BrowserStack:sessionMapping */ sessionMapping, config) {
+var BrowserStackReporter = function (logger, /* BrowserStack:sessionMapping */ sessionMapping, config) {
   try {
-
-    log = logger.create('reporter.browserstack')
+    var log = logger.create('reporter.browserstack')
     var suiteResults = []
     var buildId
-    var callWhenFinished = function() {}
+    var callWhenFinished = function () {}
 
-    var exitIfAllFinished = function() {
+    var exitIfAllFinished = function () {
       callWhenFinished()
     }
 
-    this.onRunStart = function(browsers) {
+    this.onRunStart = function (browsers) {
       // implement this function if you want to perform custom actions post the overall test batch run has started
 
-    };
+    }
 
-    this.onBrowserStart = function(browser) {
+    this.onBrowserStart = function (browser) {
       // implement this function if you want to perform custom actions post browser start
-      log.debug("Browser: " + browser.name + " has been started");
-    };
+      log.debug('Browser: ' + browser.name + ' has been started')
+    }
 
-    this.onBrowserError = function(browser, error) {
+    this.onBrowserError = function (browser, error) {
       var browserId = browser.launchId || browser.id
       if (browserId in sessionMapping) {
-        existingObj = suiteResults.find(o => o.browserId === browserId);
+        var existingObj = suiteResults.find(o => o.browserId === browserId)
         if (typeof existingObj === 'undefined') {
           suiteResults.push({
             browserId: browserId,
@@ -45,13 +43,13 @@ var BrowserStackReporter = function(logger, /* BrowserStack:sessionMapping */ se
           })
         }
       }
-    };
+    }
 
-    this.specSuccess = function(browser, result) {
+    this.specSuccess = function (browser, result) {
       try {
         var browserId = browser.launchId || browser.id
         if (browserId in sessionMapping) {
-          existingObj = suiteResults.find(o => o.browserId === browserId);
+          var existingObj = suiteResults.find(o => o.browserId === browserId)
           if (typeof existingObj === 'undefined') {
             suiteResults.push({
               browserId: browserId,
@@ -68,14 +66,14 @@ var BrowserStackReporter = function(logger, /* BrowserStack:sessionMapping */ se
       } catch (e) {
         log.debug('Browserstack reporter module specSuccess function encountered issues.\nError message: ' + e.message + '\nStacktrace: ' + e.stack)
       }
-    };
+    }
 
-    this.specFailure = this.specSkipped = function(browser, result) {
+    this.specFailure = this.specSkipped = function (browser, result) {
       try {
         var browserId = browser.launchId || browser.id
         log.debug('Spec Failure:' + JSON.stringify(result))
         if (browserId in sessionMapping) {
-          existingObj = suiteResults.find(o => o.browserId === browserId);
+          var existingObj = suiteResults.find(o => o.browserId === browserId)
           if (typeof existingObj === 'undefined') {
             suiteResults.push({
               browserId: browserId,
@@ -96,36 +94,34 @@ var BrowserStackReporter = function(logger, /* BrowserStack:sessionMapping */ se
       } catch (e) {
         log.debug('Browserstack reporter module specFailure function encountered issues.\nError message: ' + e.message + '\nStacktrace: ' + e.stack)
       }
-    };
-
-    this.onSpecComplete = function(browser, result) {
-      var browserId = browser.launchId || browser.id
-      if (result.skipped) {
-        this.specSkipped(browser, result);
-      } else if (result.success) {
-        this.specSuccess(browser, result);
-      } else {
-        this.specFailure(browser, result);
-      }
-      log.debug('Suite: ' + result.suite + ', Spec completed: ' + result.description);
     }
 
-    this.onRunComplete = function(browsersCollection, results) {
+    this.onSpecComplete = function (browser, result) {
+      if (result.skipped) {
+        this.specSkipped(browser, result)
+      } else if (result.success) {
+        this.specSuccess(browser, result)
+      } else {
+        this.specFailure(browser, result)
+      }
+      log.debug('Suite: ' + result.suite + ', Spec completed: ' + result.description)
+    }
+
+    this.onRunComplete = function (browsersCollection, results) {
       // implement this function if you want to perform custom actions post test run completion
       try {
-        log.debug("Test run completed\n" + JSON.stringify(results))
+        log.debug('Test run completed\n' + JSON.stringify(results))
         var browserId = browsersCollection.browsers[0].id
 
         if (typeof buildId === 'undefined') {
           var browserstackClient = Browserstack.createAutomateClient(sessionMapping.credentials)
-          common.getSessionDetails(log, browserstackClient, sessionMapping, browserId, config, function(sessionObj) {
-            browserUrl = sessionObj['automation_session']['browser_url']
+          common.getSessionDetails(log, browserstackClient, sessionMapping, browserId, config, function (sessionObj) {
+            var browserUrl = sessionObj['automation_session']['browser_url']
             if (typeof browserUrl !== 'undefined') {
               buildId = browserUrl.split('/').slice(-3)[0]
               var originalBuildName = config.browserStack.build
-              if (typeof buildId !== undefined || buildId != undefined) {
-                var originalBuildName = config.browserStack.build
-                common.updateBuildName(buildId, originalBuildName, results, config)
+              if (typeof buildId !== 'undefined' || buildId !== undefined) {
+                common.updateBuildName(log, buildId, originalBuildName, results, config)
               }
             }
           })
@@ -133,11 +129,11 @@ var BrowserStackReporter = function(logger, /* BrowserStack:sessionMapping */ se
       } catch (e) {
         log.debug('onRunComplete: Browserstack reporter module encountered issues.\nError message: ' + e.message + '\nStacktrace: ' + e.stack)
       }
-    };
+    }
 
-    var findSpecObjectRecords = function(browserId, specStatusSearchCondition) {
+    var findSpecObjectRecords = function (browserId, specStatusSearchCondition) {
       var suiteObj = suiteResults.find(o => o.browserId === browserId)
-      var specRecord = undefined
+      var specRecord
       if (typeof suiteObj !== 'undefined') {
         if (specStatusSearchCondition !== '') {
           specRecord = suiteObj.records.find(o => o.apiStatus === specStatusSearchCondition)
@@ -146,13 +142,13 @@ var BrowserStackReporter = function(logger, /* BrowserStack:sessionMapping */ se
       return [suiteObj, specRecord]
     }
 
-    var removeFromSuiteResults = function(objectListToRemove) {
-      suiteResults = suiteResults.filter(function(item) {
+    var removeFromSuiteResults = function (objectListToRemove) {
+      suiteResults = suiteResults.filter(function (item) {
         return item !== objectListToRemove
       })
     }
 
-    this.onBrowserComplete = function(browser) {
+    this.onBrowserComplete = function (browser) {
       try {
         var browserId = browser.launchId || browser.id
         var failedObjRecord = findSpecObjectRecords(browserId, 'Failed')
@@ -166,22 +162,22 @@ var BrowserStackReporter = function(logger, /* BrowserStack:sessionMapping */ se
           } else if (typeof errorObjRecord[1] === 'undefined') {
             var passedObjRecord = findSpecObjectRecords(browserId, 'Passed')
             if (typeof passedObjRecord[1] !== 'undefined') {
-              var reason = 'Suite: ' + passedObjRecord[1]['suite'] + ', Specs run: ' + JSON.stringify(passedObjRecord[1]['specs'])
+              reason = 'Suite: ' + passedObjRecord[1]['suite'] + ', Specs run: ' + JSON.stringify(passedObjRecord[1]['specs'])
               common.updateStatusSession(log, browserstackClient, sessionMapping, browserId, passedObjRecord[1].apiStatus, reason, passedObjRecord[1]['suite'])
               removeFromSuiteResults(passedObjRecord[0])
             }
           }
         }
-        result = browser.lastResult
-        log.debug("Browser's Last Result: " + JSON.stringify(result, null, 2))
+        var result = browser.lastResult
+        log.debug('Browser Last Result: ' + JSON.stringify(result, null, 2))
         if (result.disconnected) {
           log.error('✖ Test Disconnected')
         } else if (result.error) {
           log.error('✖ Test Errored')
         }
-        var errorObjRecord = findSpecObjectRecords(browserId, 'Error')
+        errorObjRecord = findSpecObjectRecords(browserId, 'Error')
         if (typeof errorObjRecord[1] !== 'undefined') {
-          var reason = 'Suite: ' + errorObjRecord[1]['suite'] + ', Error log: ' + errorObjRecord[1]['log']
+          reason = 'Suite: ' + errorObjRecord[1]['suite'] + ', Error log: ' + errorObjRecord[1]['log']
           common.updateStatusSession(log, browserstackClient, sessionMapping, browserId, errorObjRecord[1].apiStatus, reason, errorObjRecord[1]['suite'])
           removeFromSuiteResults(errorObjRecord[0])
         }
@@ -191,14 +187,12 @@ var BrowserStackReporter = function(logger, /* BrowserStack:sessionMapping */ se
     }
 
     // Wait until all updates have been pushed to Browserstack
-    this.onExit = function(done) {
+    this.onExit = function (done) {
       callWhenFinished = done
       exitIfAllFinished()
     }
-
   } catch (e) {
     log.debug('Browserstack reporter full module encountered issues.\nError message: ' + e.message + '\nStacktrace: ' + e.stack)
-
   }
 }
 // PUBLISH DI MODULE
--- common.js
+++ common.js
@@ -1,53 +1,53 @@
-var RestClient = require('node-rest-client').Client;
+var RestClient = require('node-rest-client').Client
 var base64js = require('base64-js')
 
 module.exports = {
-  updateStatusSession: function(log, browserstackClient, sessionMapping, browserId, status, reason, sessionName) {
+  updateStatusSession: function (log, browserstackClient, sessionMapping, browserId, status, reason, sessionName) {
     browserstackClient.updateSession(sessionMapping[browserId], {
       status: status,
       reason: reason,
       name: sessionName.length !== 0 ? sessionName.toString() : 'Karma Test Suite'
-    }, function(error) {
+    }, function (error) {
       if (error) {
         log.error('✖ Could not update BrowserStack status' + error)
       }
     })
   },
-  getSessionDetails: function(log, browserstackClient, sessionMapping, browserId, config, callback) {
+  getSessionDetails: function (log, browserstackClient, sessionMapping, browserId, config, callback) {
     try {
-      apiClientEndpoint = config.browserStack.apiClientEndpoint
-      var creds = sessionMapping.credentials.username + ":" + sessionMapping.credentials.password
-      var arr = [];
+      var apiClientEndpoint = config.browserStack.apiClientEndpoint
+      var creds = sessionMapping.credentials.username + ':' + sessionMapping.credentials.password
+      var arr = []
       for (var i = 0; i < creds.length; i++) {
-        arr.push(creds.charCodeAt(i));
+        arr.push(creds.charCodeAt(i))
       }
-      encodedCreds = base64js.fromByteArray(arr);
-      var client = new RestClient();
+      var encodedCreds = base64js.fromByteArray(arr)
+      var client = new RestClient()
       var args = {
         data: {},
         headers: {
-          "Authorization": "Basic " + encodedCreds
+          'Authorization': 'Basic ' + encodedCreds
         }
-      };
-      client.get(apiClientEndpoint + "/automate/sessions/" + sessionMapping[browserId] + ".json", args, function(data, response) {
+      }
+      client.get(apiClientEndpoint + '/automate/sessions/' + sessionMapping[browserId] + '.json', args, function (data, response) {
         callback(data)
-      });
+      })
     } catch (e) {
       log.debug('Browserstack reporter module encountered issues.\nError message: ' + e.message + '\nStacktrace: ' + e.stack)
     }
   },
-  updateBuildName: function(buildId, originalBuildName, results, config) {
+  updateBuildName: function (log, buildId, originalBuildName, results, config) {
     try {
-      log.debug("Attempting build name update")
+      log.debug('Attempting build name update')
       if (typeof buildId === 'undefined') {
-        log.debug("BrowserStack build id is not defined. Not attempting update for build id!")
+        log.debug('BrowserStack build id is not defined. Not attempting update for build id!')
         return
       }
 
-      apiClientEndpoint = config.browserStack.apiClientEndpoint
-      var client = new RestClient();
+      var apiClientEndpoint = config.browserStack.apiClientEndpoint
+      var client = new RestClient()
       var newBuildName
-      t = new Date().toISOString()
+      var t = new Date().toISOString()
 
       if (originalBuildName === null || typeof originalBuildName === 'undefined') {
         originalBuildName = 'Karma build'
@@ -56,43 +56,40 @@ module.exports = {
       if (typeof results !== 'undefined') {
         if (results.exitCode === 1) {
           if (results.disconnected === true) {
-            newBuildName = originalBuildName + " completed at " + t + " with 1 or more session disconnects: [Specs passed: " + results.success + " | Specs failed: " + results.failed + "]"
+            newBuildName = originalBuildName + ' completed at ' + t + ' with 1 or more session disconnects: [Specs passed: ' + results.success + ' | Specs failed: ' + results.failed + ']'
           } else if (results.error === true) {
-            newBuildName = originalBuildName + " completed at " + t + " with 1 or more session errors: [Specs passed: " + results.success + " | Specs failed: " + results.failed + "]"
+            newBuildName = originalBuildName + ' completed at ' + t + ' with 1 or more session errors: [Specs passed: ' + results.success + ' | Specs failed: ' + results.failed + ']'
+          } else if (results.failed !== 0) {
+            newBuildName = originalBuildName + ' completed at ' + t + ' with spec failures: [Specs passed: ' + results.success + ' | Specs failed: ' + results.failed + ']'
           }
-          else if (results.failed !== 0) {
-            newBuildName = originalBuildName + " completed at " + t + " with spec failures: [Specs passed: " + results.success + " | Specs failed: " + results.failed + "]"
-          }
-        } else if (results.failed == 0) {
-          newBuildName = originalBuildName + " completed successfully at " + t + ": [Specs passed: " + results.success + "]"
+        } else if (results.failed === 0) {
+          newBuildName = originalBuildName + ' completed successfully at ' + t + ': [Specs passed: ' + results.success + ']'
         }
       } else {
-        newBuildName = originalBuildName + " started at " + t + " | In Progress | "
+        newBuildName = originalBuildName + ' started at ' + t + ' | In Progress | '
       }
-      var creds = config.browserStack.username + ":" + config.browserStack.accessKey
-      var arr = [];
+      var creds = config.browserStack.username + ':' + config.browserStack.accessKey
+      var arr = []
       for (var i = 0; i < creds.length; i++) {
-        arr.push(creds.charCodeAt(i));
+        arr.push(creds.charCodeAt(i))
       }
-      encodedCreds = base64js.fromByteArray(arr);
+      var encodedCreds = base64js.fromByteArray(arr)
       var args = {
         data: {
           name: newBuildName
         },
         headers: {
-          "Authorization": "Basic " + encodedCreds,
-          "Content-Type": "application/json"
+          'Authorization': 'Basic ' + encodedCreds,
+          'Content-Type': 'application/json'
         }
-      };
-      var completeEndpoint = apiClientEndpoint + "/automate/builds/" + buildId + ".json"
-      log.debug("Complete endpoint for build name update: " + completeEndpoint)
-      client.put(completeEndpoint, args, function(data, response) {
-      });
-
-      log.debug("Exiting updateBuildName function")
-
+      }
+      var completeEndpoint = apiClientEndpoint + '/automate/builds/' + buildId + '.json'
+      log.debug('Complete endpoint for build name update: ' + completeEndpoint)
+      client.put(completeEndpoint, args, function (data, response) {
+      })
+      log.debug('Exiting updateBuildName function')
     } catch (e) {
       log.debug('Browserstack reporter module update build name function encountered issues.\nError message: ' + e.message + '\nStacktrace: ' + e.stack)
     }
   }
-};
+}
